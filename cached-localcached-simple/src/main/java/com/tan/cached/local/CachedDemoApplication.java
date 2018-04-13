d4e6670a72eef5021235a6cf574d0a5eb9cf2751
tanzanlong <15218835680@163.com>
2018-04-13 17:40:05 +0800

    引用

--- cached-localcached-simple/src/main/java/com/tan/cached/local/CachedDemoApplication.java
+++ cached-localcached-simple/src/main/java/com/tan/cached/local/CachedDemoApplication.java
@@ -1,8 +1,5 @@
 package com.tan.cached.local;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
